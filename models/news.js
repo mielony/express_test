7f462c1b2341ba67593b7012174f203b2b6369b5
mielony <mielniczuk.mariusz@gmail.com>
2022-01-31 16:55:18 +0100

    List of articles + ability to delete them

--- models/news.js
+++ models/news.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema  = mongoose.Schema;
 
   const newsSchema = new Schema({
-    title:  {type: String, required: true}, // String is shorthand for {type: String}
-    author: String,
-    description:   {type: String, required: true},
+    title:  {type: String, required: [true, 'Pole tytul jest wymagane']}, // String is shorthand for {type: String}
+    author: {type: String, required: [true, 'Pole autor jest wymagane']}, 
+    description:   {type: String},
     created: { type: Date, default: Date.now },
   });
 
--- routes/admin.js
+++ routes/admin.js
@@ -14,22 +14,45 @@ router.all('*', (req, res, next) => {
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  // Dodany na sztywno pierwszy news :) 
-  // const newsData = new News({
-  //   title:  "Testowa informacja",
-  //   author: "Mariusz Mielniczuk",
-  //   description:   "Jest to jakis opis tego artyk.",
-  // });
-  // newsData.save((err) => {
-  //   console.log("Blad zapisu do bazy danych", err);
-  // })
-  res.render('admin', { title: 'Administracja' });
+  News.find({}, (err, data) => { 
+    console.log(data);
+    res.render('admin/index', { title: 'Administracja', data});
+  });
+  
+});
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    if (err) console.log("Blad przy usuwaniu", err);
+    res.redirect('/admin');
+  })
 });
 
 router.get('/news/add', (req, res) => {
-  
+ // const errors = {errors: ['', '']};
+  res.render('admin/news-form', {title: 'Dodaj newsa', errors: {}, body:{} });
 })
+ 
 
+router.post('/news/add', (req, res) => {
+  const body = req.body;
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+  console.log("Errors**************", errors);
+  if (errors !== undefined) {
+    res.render('admin/news-form', {title: 'Dodaj newsa', errors, body});
+  } 
+  else {
+    newsData.save((err) => {
+      if (err) (console.log('Problem z zapisem do bazy', err)) });
+    res.redirect('/admin');
+  }
+  //
+});
+
+router.get('/admin', (req, res) => {
+
+});
 
 module.exports = router;
-  
+   
