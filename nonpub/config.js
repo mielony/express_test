8e21df94d8b1701898e6f4a96e3f11eb9cb750b8
Mariusz Mielniczuk <mielniczuk.mariusz@gmail.com>
2022-02-01 15:45:16 +0100

    Delete config.js

--- nonpub/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://mielony:bFAIZ8TpITLtPmOD@cluster0.n2rvb.mongodb.net/cms',
-    keySession: ['MOJKLUCZDOSESJI'],
-    maxAgeSession: 1 * 60 * 60 * 1000, 
-} 
